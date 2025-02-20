4e2b7561f2c4bf77031879dfafab14ea08dab0cc
JanisBramanisVTDTipb22 <127381689+JanisBramanisVTDTipb22@users.noreply.github.com>
2025-02-20 09:04:07 +0200

    first commit

---
+++ src/Translations.js
@@ -0,0 +1,68 @@
+function Translation() {
+    return (
+      
+        <table>
+        <caption>
+        Gherkin termini 
+        </caption>
+        <thead>
+          <tr>
+            <th scope="col">English</th>
+            <th scope="col">Latviešu</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td scope="row">Feature</td>
+            <td>Funkcionālā iezīme</td>
+          </tr>
+          <tr>
+            <td scope="row">Background</td>
+            <td>Konteksts</td>
+          </tr>
+          <tr>
+            <td scope="row">Scenario</td>
+            <td>Scenārijs</td>
+          </tr>
+          <tr>
+            <td scope="row">Scenario Outline</td>
+            <td>Scenārijs pēc parauga</td>
+          </tr>
+          <tr>
+            <td scope="row">Examples</td>
+            <td>Piemēri</td>
+          </tr>
+          <tr>
+            <td scope="row">Given</td>
+            <td>Kad</td>
+          </tr>
+          <tr>
+            <td scope="row">When</td>
+            <td>Ja</td>
+          </tr>
+          <tr>
+            <td scope="row">Then</td>
+            <td>Tad</td>
+          </tr>
+          <tr>
+            <td scope="row">And</td>
+            <td>Un</td>
+          </tr>
+          <tr>
+            <td scope="row">But</td>
+            <td>Bet</td>
+          </tr>
+          <tr>
+            <td scope="row">Or</td>
+            <td>Vai</td>
+          </tr>
+        </tbody>
+      </table>
+
+
+
+    );
+  }
+
+  
+  export default Translation
